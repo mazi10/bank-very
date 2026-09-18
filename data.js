8ef4e26ca7b3473e6b00c18aecf7bc695677fac8
Mujammal mazi <mujammalmajl45@gmail.com>
2026-09-18 21:55:18 +0600

    Added new employee data

--- data.js
+++ data.js
@@ -30,4 +30,14 @@ const employeesData = {
         date: "18-Sep-2026",
         statusText: "Verification success!!!"
     },
+        "110": {
+        headerHtml: `<p><span style="color: rgb(230, 0, 0); font-size: 18px;">UCB</span><span style="color: rgb(230, 0, 0);"> </span><span style="color: rgb(230, 0, 0); font-size: 14px;">Bank P.L.C</span><span style="color: rgb(230, 0, 0); font-size: 14px;" class="ql-font-georgia"><span class="ql-cursor">﻿</span></span></p>`,
+        fontFamily: "Arial, sans-serif",
+        accountNo: "1282112000002446",
+        name: "AHAMMAD MAZI",
+        amount: "5,118,827.42",
+        currentBalance: "1,200,500.00",
+        date: "18-Sep-2026",
+        statusText: "Verification success!!!"
+    },
 };
