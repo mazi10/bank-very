3afe3f1bb2651e54a7145155465844f495c23161
Mujammal mazi <mujammalmajl45@gmail.com>
2026-09-18 21:46:34 +0600

    Added new employee data

--- data.js
+++ data.js
@@ -10,4 +10,14 @@ const employeesData = {
         date: "18-Sep-2026",
         statusText: "Verification success!!!"
     },
+        "909": {
+        headerHtml: `<p>Ucb bank plc</p><p><br></p><p>Munshiganj, dhaka</p><p><br></p><p>Tongibari Munshiganj</p>`,
+        fontFamily: "Arial, sans-serif",
+        accountNo: "1282112000002446",
+        name: "AHAMMAD MAZI",
+        amount: "5,118,827.42",
+        currentBalance: "1,200,500.00",
+        date: "18-Sep-2026",
+        statusText: "Verification success!!!"
+    },
 };
