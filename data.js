dba19c60560e936a681017638ea40e1923362967
Mujammal mazi <mujammalmajl45@gmail.com>
2026-09-18 21:20:18 +0600

    Added new employee data

--- data.js
+++ data.js
@@ -1,4 +1,23 @@
 const employeesData = {
     // admin.html থেকে জেনারেট করা কোড এখানে পেস্ট করবেন
-         
+     "1": {
+        headerHtml: `<h1><span style="font-size: 18px; color: rgb(0, 102, 204);">Ucb bank plc</span></h1>`,
+        fontFamily: "Georgia, serif",
+        accountNo: "1282112000002446",
+        name: "AHAMMAD MAZI",
+        amount: "5,118,827.42",
+        currentBalance: "1,200,500.00",
+        date: "18-Sep-2026",
+        statusText: "Verification success"
+    },
+        "22": {
+        headerHtml: `<h1><span style="font-size: 18px; color: rgb(0, 102, 204);">Ucb bank plc</span></h1>`,
+        fontFamily: "Georgia, serif",
+        accountNo: "1282112000002446",
+        name: "AHAMMAD MAZI",
+        amount: "5,118,827.42",
+        currentBalance: "1,200,500.00",
+        date: "18-Sep-2026",
+        statusText: "Verification success"
+    },
 };
