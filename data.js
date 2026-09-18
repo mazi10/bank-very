182942b3d11e691c0c56dcb0846441730a721cd3
Mujammal mazi <mujammalmajl45@gmail.com>
2026-09-18 20:45:55 +0600

    Added new employee data

--- data.js
+++ data.js
@@ -1,14 +1,4 @@
 const employeesData = {
     // admin.html থেকে জেনারেট করা কোড এখানে পেস্ট করবেন
-         "1861570025384": {
-        name: "AHAMMAD MAZI",
-        amount: "5,118,827.42",
-        date: "18-Sep-2026",
-        htext: "Dutch bangla bank plc",
-        hsize: "22px",
-        hcolor: "#2563eb",
-        halign: "center",
-        lurl: "",
-        lsize: "45px"
-    },
+         
 };
