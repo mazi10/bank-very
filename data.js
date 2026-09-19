a0a75f5d0c863ad69bd9f19de592f1c523558101
Mujammal mazi <mujammalmajl45@gmail.com>
2026-09-19 16:19:26 +0600

    Added new employee data

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const employeesData = {
     // admin.html থেকে জেনারেট করা কোড এখানে পেস্ট করবেন
-      "9090": {
-        headerHtml: `<p>Ucb bank plc</p><p><br></p><p>Munshiganj, dhaka</p><p><br></p><p>Tongibari Munshiganj</p>`,
+         "223": {
+        headerHtml: `<h1><em style="font-size: 24px; color: rgb(0, 102, 204);">ASIA BANK PLC</em></h1>`,
         fontFamily: "Arial, sans-serif",
         accountNo: "1282112000002446",
         name: "AHAMMAD MAZI",
@@ -10,34 +10,6 @@ const employeesData = {
         date: "18-Sep-2026",
         statusText: "Verification success!!!"
     },
-        "909": {
-        headerHtml: `<p>Ucb bank plc</p><p><br></p><p>Munshiganj, dhaka</p><p><br></p><p>Tongibari Munshiganj</p>`,
-        fontFamily: "Arial, sans-serif",
-        accountNo: "1282112000002446",
-        name: "AHAMMAD MAZI",
-        amount: "5,118,827.42",
-        currentBalance: "1,200,500.00",
-        date: "18-Sep-2026",
-        statusText: "Verification success!!!"
-    },
-        "110": {
-        headerHtml: `<p><span style="color: rgb(230, 0, 0); font-size: 18px;">UCB</span><span style="color: rgb(230, 0, 0);"> </span><span style="color: rgb(230, 0, 0); font-size: 14px;">Bank P.L.C</span><span style="color: rgb(230, 0, 0); font-size: 14px;" class="ql-font-georgia"><span class="ql-cursor">﻿</span></span></p>`,
-        fontFamily: "Arial, sans-serif",
-        accountNo: "1282112000002446",
-        name: "AHAMMAD MAZI",
-        amount: "5,118,827.42",
-        currentBalance: "1,200,500.00",
-        date: "18-Sep-2026",
-        statusText: "Verification success!!!"
-    },
-        "110": {
-        headerHtml: `<p><span style="color: rgb(230, 0, 0); font-size: 18px;">UCB</span><span style="color: rgb(230, 0, 0);"> </span><span style="color: rgb(230, 0, 0); font-size: 14px;">Bank P.L.C</span><span style="color: rgb(230, 0, 0); font-size: 14px;" class="ql-font-georgia"><span class="ql-cursor">﻿</span></span></p>`,
-        fontFamily: "Arial, sans-serif",
-        accountNo: "1282112000002446",
-        name: "AHAMMAD MAZI",
-        amount: "5,118,827.42",
-        currentBalance: "1,200,500.00",
-        date: "18-Sep-2026",
-        statusText: "Verification success!!!"
-    },
+       
+        
 };
